cd382a1bd5657fc33a0a88b20a5f6d82f1966ba7
artem00611 <artem006661artem@gmail.com>
2021-05-09 20:06:53 +0300

    Fixed bugs. Refactored code

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -5,7 +5,6 @@ import "node_modules/openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
 import 'node_modules/openzeppelin-solidity/contracts/access/Ownable.sol';
 
 contract Bank is Ownable{
-    using SafeMath for uint;
     using SafeMath for uint256;
 
     struct DepositType{
@@ -23,69 +22,81 @@ contract Bank is Ownable{
         uint256 amount;
         uint256 percentage;
     }
-    DepositType[] d;
+
+    DepositType[] depositType;
 
     MyToken token;
-    uint256 monthSeconds = 86400;
-    uint256 ammountCreditPay;
     mapping (address => DepositInfo[]) private deposits;
     mapping (address => CreditInfo) private credits;
     mapping (address => bool) private whitelist;
     uint256 creditPercentage;
+    uint256 accuraccy = 1000000000000000000;
 
     constructor(){
-        d.push(DepositType(15778463, 300));
-        d.push(DepositType(31556926, 600));
-        creditPercentage = 1000;
+        depositType.push(DepositType(15778463, 3 * accuraccy));
+        depositType.push(DepositType(31556926, 6 * accuraccy));
+        creditPercentage = 10 * accuraccy;
+    }
+
+    function getTokensFromContract(uint256 amount) public onlyOwner{
+        token.transfer(msg.sender, amount);
     }
 
     function addNewDepositType(uint256 time, uint256 percentage) public onlyOwner{
-        d.push(DepositType(time, percentage));
+        depositType.push(DepositType(time, percentage));
+    }
+
+    function deleteDepositType(uint256 depositTypeId) public onlyOwner{
+        delete depositType[depositTypeId];
+        token.decimals();
     }
 
     function changeCreditPercentage(uint256 changedPercentage) public onlyOwner{
         creditPercentage = changedPercentage;
     }
 
-    function openDeposit(uint256 amount, uint256 depositTypeId) public payable{
-        DepositType[] storage depositType = d;
+    function openDeposit(uint256 amount, uint256 depositTypeId) public{
         require(depositType[depositTypeId].percentage > 0, "Can`t find deposit with this type id");
         token.transferFrom(msg.sender, address(this), amount);
-        deposits[msg.sender].push(DepositInfo(block.timestamp.add(depositType[depositTypeId].depositTime),
-         amount, depositType[depositTypeId].percentage));
+        deposits[msg.sender].push(DepositInfo(block.timestamp.add(depositType[depositTypeId].depositTime), amount, depositType[depositTypeId].percentage));
     }
 
     function withdrawDeposit(uint256 depositId) public{
         require(deposits[msg.sender][depositId].endTime > block.timestamp, "You can`t withdraw your deposit");
-        token.transfer(msg.sender,
-         deposits[msg.sender][depositId].sum
+        uint256 depositAmmount = deposits[msg.sender][depositId].sum
          .mul(deposits[msg.sender][depositId].percentage
-         .div(10000)));
+         .div(accuraccy));
+        token.transfer(msg.sender, depositAmmount);
         delete deposits[msg.sender][depositId];
     }
 
-    function getCredit(uint256 ammountCredit) public{
+    function getCredit(uint256 amountCredit) public{
         require(whitelist[msg.sender], "You don`t have permission to take credit!");
         require(credits[msg.sender].amount > 0, "You already have credit!");
-        credits[msg.sender] = (CreditInfo(block.timestamp, ammountCredit, creditPercentage));
-        token.transfer(msg.sender, ammountCredit);
+        require(amountCredit > 0, "You can`t get credit equal to zero!");
+        credits[msg.sender] = (CreditInfo(block.timestamp, amountCredit, creditPercentage));
+        token.transfer(msg.sender, amountCredit);
     }
 
     function changeAccountCreditPermission(address account, bool creditPermission) public onlyOwner{
         whitelist[account] = creditPermission;
     }
 
-    function payOffCredit() public payable{
+    function payOffCredit() public{
+        uint256 amountCreditPay;
+        uint256 monthSeconds = 86400;
         require(credits[msg.sender].amount > 0, "You don`t have credit!");
-        ammountCreditPay = (credits[msg.sender].amount
-        .mul(creditPercentage
-        .mul((block.timestamp
-        .sub(credits[msg.sender].startTime))
+
+        amountCreditPay = credits[msg.sender].amount
+        .add((credits[msg.sender].amount
+        .mul(creditPercentage)
+        .div(accuraccy))
+        .mul((block.timestamp - credits[msg.sender].startTime)
+        .mul(accuraccy))
         .div(monthSeconds))
-        .div(10000)))
-        .add(credits[msg.sender].amount);
-        require(ammountCreditPay > token.balanceOf(msg.sender), "You don`t have enough KepCoins to pay off credit!");
-        token.transferFrom(msg.sender, address(this), ammountCreditPay);
+        .div(accuraccy);
+        require(amountCreditPay > token.balanceOf(msg.sender), "You don`t have enough KepCoins to pay off credit!");
+        token.transferFrom(msg.sender, address(this), amountCreditPay);
         delete credits[msg.sender];
     }
 }
