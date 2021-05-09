0637fb9634a3324adc0f6d7cdd92b669884285df
artem00611 <artem006661artem@gmail.com>
2021-05-09 16:34:57 +0300

    Added credit feature

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.8.0;
 
 import "./MyToken.sol";
 import "node_modules/openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
+import 'node_modules/openzeppelin-solidity/contracts/access/Ownable.sol';
 
-
-contract Bank{
+contract Bank is Ownable{
     using SafeMath for uint;
     using SafeMath for uint256;
 
@@ -18,13 +18,23 @@ contract Bank{
         uint256 percentage;
     }
     
-
+    struct CreditInfo{
+        uint256 startTime;
+        uint256 ammout;
+        uint256 percentage;
+    }
     DepositType[] d;
 
     MyToken token;
+    uint256 monthSeconds = 86400;
+    uint256 ammountCreditPay;
     mapping (address => DepositInfo[]) private deposits;
-
+    mapping (address => CreditInfo) private credits;
     uint256 creditPercentage = 1000;
+
+    function changeCreditPercentage(uint256 changedPercentage) public onlyOwner{
+        creditPercentage = changedPercentage;
+    }
 
     function openDeposit(uint256 amount, uint256 depositTypeId) public payable{
         d.push(DepositType(15778463, 300));
@@ -48,4 +58,18 @@ contract Bank{
         return token.balanceOf(msg.sender);
     }
 
+    function getCredit(uint256 ammountCredit) public{
+        require(token.balanceOf(msg.sender) > 5, "You don`t have enough KepCoins to take credits!");
+        require(credits[msg.sender].ammout > 0, "You already have credit!");
+        credits[msg.sender] = (CreditInfo(block.timestamp, ammountCredit, creditPercentage));
+        token.transfer(msg.sender, ammountCredit);
+    }
+
+    function payOffCredit() public payable{
+        require(credits[msg.sender].ammout > 0, "You don`t have credit!");
+        ammountCreditPay = credits[msg.sender].ammout.mul(creditPercentage.mul((block.timestamp.sub(credits[msg.sender].startTime)).div(monthSeconds)).div(10000));
+        require(ammountCreditPay > token.balanceOf(msg.sender), "You don`t have credit!");
+        token.transferFrom(msg.sender, address(this), ammountCreditPay);
+        delete credits[msg.sender];
+    }
 }
